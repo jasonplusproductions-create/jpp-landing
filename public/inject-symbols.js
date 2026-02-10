5a31967212b17f0b0c19965d4bc0d378ce2981ae
jaydhac-png <jaydhac@gmail.com>
2026-02-10 15:31:24 -0500

    Deploy ARRANGEMENTS symbols

---
+++ public/inject-symbols.js
@@ -0,0 +1,11 @@
+﻿(() => {
+  fetch('/symbols.svg')
+    .then(r => r.text())
+    .then(svg => {
+      const wrap = document.createElement('div');
+      wrap.style.display = 'none';
+      wrap.innerHTML = svg;
+      document.body.prepend(wrap);
+    })
+    .catch(console.error);
+})();
